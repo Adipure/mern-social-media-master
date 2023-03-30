2e96d07ad4d259a054369debee37dcacfd5dbf48
chanwoo Hwang <chanwoo1017@hotmail.com>
2023-03-29 20:48:17 -0700

    commi

--- server/index.js
+++ server/index.js
@@ -51,6 +51,9 @@ app.post("/posts", verifyToken, upload.single("picture"), createPost);
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
+app.get('*', (req, res) => {
+  res.sendFile(join(__dirname, '../client', 'build', 'index.html'))
+})
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
