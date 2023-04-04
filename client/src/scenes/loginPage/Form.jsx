5443fc475276a037ecfc75159989caf06e5f010c
chanwoo Hwang <chanwoo1017@hotmail.com>
2023-04-03 18:51:21 -0700

    update

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -21,9 +21,9 @@ const registerSchema = yup.object().shape({
   lastName: yup.string().required("required"),
   email: yup.string().email("invalid email").required("required"),
   password: yup.string().required("required"),
-  location: yup.string().required("required"),
-  occupation: yup.string().required("required"),
-  picture: yup.string().required("required"),
+  location: yup.string(),
+  occupation: yup.string(),
+  picture: yup.string(),
 });
 
 const loginSchema = yup.object().shape({
